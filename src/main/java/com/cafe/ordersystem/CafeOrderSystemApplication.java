5b850968d4d1ee1cb0ee5bae7652d40417621ce5
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-04-09 23:46:09 +0200

    Initial commit

---
+++ src/main/java/com/cafe/ordersystem/CafeOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cafe.ordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafe/ordersystem/CafeOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
