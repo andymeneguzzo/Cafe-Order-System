30295d0c63ef02d1cab22a05d016936ae5e1731b
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-04-10 11:07:35 +0200

    create auditable entity

--- src/main/java/com/cafe/ordersystem/model/common/AuditableEntity.java
+++ src/main/java/com/cafe/ordersystem/model/common/AuditableEntity.java
@@ -1,5 +1,18 @@
 package com.cafe.ordersystem.model.common;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
 /**
  * Base class for entities that need to be audited.
  *
@@ -11,5 +24,26 @@ package com.cafe.ordersystem.model.common;
  *
  * It uses Spring Data JPA's auditing capabilities via the @EntityListeners annotation.
  */
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
 public abstract class AuditableEntity extends BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = true, updatable = false, length = 50)
+    private String createdBy;
+
+    @LastModifiedDate
+    @Column(nullable = true)
+    private LocalDateTime updatedAt;
+
+    @LastModifiedBy
+    @Column(nullable = true, length = 50)
+    private String updatedBy;
 }
