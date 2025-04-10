f5af8bd2fa3481705886bbaefa7c78ff83d54581
Andrés Christian Meneguzzo <andres.meneguzzo@gmail.com>
2025-04-10 11:03:34 +0200

    create base entity

--- src/main/java/com/cafe/ordersystem/model/common/BaseEntity.java
+++ src/main/java/com/cafe/ordersystem/model/common/BaseEntity.java
@@ -1,5 +1,10 @@
 package com.cafe.ordersystem.model.common;
 
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Base class for all entities in the system.
  *
@@ -9,5 +14,19 @@ package com.cafe.ordersystem.model.common;
  * Entities inheriting from this class should define their own ID field
  * because different entities might have different ID types or generation strategies.
  */
+
+@MappedSuperclass
+@Getter
+@Setter
 public abstract class BaseEntity {
+
+    /**
+     * Version field for optimistic locking.
+     *
+     * When an entity is updated, Spring Data JPA checks if the version in the database
+     * matches the version in memory. If not, it means another process has modified
+     * the entity since it was loaded, and an OptimisticLockingFailureException is thrown.
+     */
+    @Version
+    private Long version;
 }
